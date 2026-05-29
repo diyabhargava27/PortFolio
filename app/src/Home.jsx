e12413b4a42f567370eba466d1ec6608ee2a625c
Diya Bhargava <117004051+diyabhargava27@users.noreply.github.com>
2026-05-29 21:41:37 +0530

    Update Home.jsx

--- app/src/Home.jsx
+++ app/src/Home.jsx
@@ -84,7 +84,7 @@ const Home = () => {
           className="mt-6 px-6 py-2 bg-cyan-500 text-black rounded-full font-bold"
           onClick={() => {
             const link = document.createElement("a");
-            link.href = "/DiyaBhargava.pdf"; 
+            link.href = "/Diya_Bhargava(heizen).pdf"; 
             link.download = "DiyaBhargava.pdf"; 
             document.body.appendChild(link);
             link.click();
